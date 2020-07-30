0abda975ff2c44108adfb76683fd87bc3443d650
Yanone <post@yanone.de>
2020-07-30 15:50:26 +0200

    Reorganized build script

--- sources/build.sh
+++ sources/build.sh
@@ -1,31 +1,39 @@
 #!/bin/sh
 set -e
 
- echo "Generating Static fonts"
- mkdir -p ../fonts/TTF/
- fontmake -g Sansita-Swashed.glyphs -i -o ttf --output-dir ../fonts/TTF/
- 
- echo "Generating VFs"
- fontmake -g Sansita-Swashed.glyphs -o variable --output-path ../fonts/TTF/Sansita-Swashed\[wght\].ttf
- 
- rm -rf master_ufo/ instance_ufo/
- echo "Post processing"
 
 
-ttfs=$(ls ../fonts/TTF/*.ttf)
+echo "Generating Static fonts"
+mkdir -p ../fonts/ttf/
+rm -rf ../fonts/ttf/*.ttf
+fontmake -g Sansita-Swashed.glyphs -i -o ttf --output-dir ../fonts/ttf/
+
+echo "Post processing"
+ttfs=$(ls ../fonts/ttf/*.ttf)
 for ttf in $ttfs
 do
 	gftools fix-dsig -f $ttf;
-	./ttfautohint-vf $ttf "$ttf.fix";
-	mv "$ttf.fix" $ttf;
+	ttfautohint $ttf $ttf.fix;
+	mv $ttf.fix $ttf;
+	gftools fix-hinting $ttf
+	mv $ttf.fix $ttf;
 done
 
-vfs=$(ls ../fonts/TTF/*-VF.ttf)
+
+
+echo "Generating VFs"
+mkdir -p ../fonts/vf/
+rm -rf ../fonts/vf/*.ttf
+fontmake -g Sansita-Swashed.glyphs -o variable --output-path ../fonts/vf/SansitaSwashed-Italic\[wght\].ttf
+
+echo "Post processing"
+vfs=$(ls ../fonts/vf/*.ttf)
 for vf in $vfs
 do
 	gftools fix-dsig -f $vf;
-	./ttfautohint-vf --stem-width-mode nnn $ttf "$ttf.fix";
-	mv "$vf.fix" $vf;
+	gftools fix-unwanted-tables $vf
+	gftools fix-nonhinting $vf $vf.fix
+	mv $vf.fix $vf;
 done
 
 gftools fix-vf-meta $vfs;
@@ -34,5 +42,11 @@ do
 	mv "$vf.fix" $vf;
 done
 
+rm ../fonts/vf/*backup*.ttf
+
+
+
 echo "QAing"
-gftools qa ../fonts/TTF/*\[wght\].ttf -gf -o ../qa --fontbakery --diffenator --diffbrowsers
+gftools qa -f ../fonts/vf/*.ttf -fb ../fonts/vf/*.ttf -o ../qa --fontbakery --diffenator --diffbrowsers
+
+rm -rf master_ufo/ instance_ufo/
